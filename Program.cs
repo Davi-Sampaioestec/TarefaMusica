1a88d361037ae8efede4c3692c4dcf8b089108f9
Davi-Sampaioestec <dixxgabriellasam@gmail.com>
2025-03-08 16:56:52 -0300

    Tarefa Finalizada

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿Console.Clear();
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("Solo - Frank Ocean");
+
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.WriteLine(@"Hand me a towel, I'm dirty dancing by myself
+Gone off tabs of that acid
+Form me a circle, watch my jagger
+Might lose my jacket and hit a solo
+One time
+We too loud in public then police turned down the function
+Now we outside and the timing's perfect
+Forgot to tell you, gotta tell you how much I vibe with you
+And we don't gotta be solo
+Now stay away from highways
+My eyes like them red lights
+Right now, I prefer yellow
+Redbone, so mellow
+Fuck 'round, be cutting you
+Think we were better off solo
+I got that act right in the Windy city that night
+No trees to blow through
+But blow me and I owe you
+Two grams when the sunrise
+Smoking good, rolling solo
+Solo (solo)
+Solo (solo)
+S-solo (solo)
+S-solo (solo)");
+
+Console.WriteLine();
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine(@"It's hell on Earth and the city's on fire
+Inhale, inhale, there's heaven
+There's a bull and a matador dueling in the sky
+Inhale, in hell there's heaven
+Oh, oh, oh, oh
+Oh, oh, oh, oh
+Solo, solo
+Solo, solo");
+
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.WriteLine(@"I'm skipping showers and switching socks
+Sleeping good and long
+Bones feeling dense as fuck, wish a nigga would cross
+And catch a solo, on time
+White leaf on my boxers, green leaf turn to vapors for the low
+And that mean cheap, 'cause ain't shit free and I know it
+Even love ain't, 'cause this nut cost, that clinic kill my soul
+But you gotta hit the pussy raw though
+Now your baby momma ain't so vicious
+All she want is her picket fence
+And you protest and you picket sign
+But then courts won't side with you
+Won't let you fly solo
+I wanted that act right in Colorado that night
+I brought trees to blow through, but it's just me and no you
+Stayed up 'til my phone died, smoking big, rolling solo");
+
+Console.WriteLine();
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine(@"It's hell on Earth and the city's on fire
+Inhale, inhale, there's heaven
+There's a bull and a matador dueling in the sky
+Inhale, in hell there's heaven
+Oh, oh, oh, oh
+Oh, oh, oh, oh
+Solo, solo
+Solo, solo");
+
+Console.WriteLine();Console.ResetColor();
+
+Console.BackgroundColor = ConsoleColor.DarkYellow;
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine(@"By myself
+Solo, solo
+Solo, solo
+(By myself, by myself)
+Solo, solo
+Solo, solo");
+
+Console.ResetColor();
